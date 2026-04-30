6b3be3908a079ee0ac403e23b13613ce1ba82341
Guangtao Zhu <guangtaozhu@outlook.com>
2026-04-30 11:49:55 +1000

    try to make the passive_server example make sense first
    Signed-off-by: Guangtao Zhu <guangtaozhu@outlook.com>

--- multikernel-passive-server/client_core0.c
+++ multikernel-passive-server/client_core0.c
@@ -7,20 +7,23 @@
 #include <microkit.h>
 
 #define SERVER_CH 0
+#define REMOTE_CH 1
 
 void init(void)
 {
     microkit_dbg_puts("CLIENT_CORE0|INFO: init function running\n");
 
-    /* message the server */
-    int runs = 5;
-    while (runs--) {
-        microkit_dbg_puts("client 0 - call server on 0\n");
-        (void) microkit_ppcall(SERVER_CH, microkit_msginfo_new(1, 1));
-    }
+    microkit_notify(REMOTE_CH);
 }
 
 void notified(microkit_channel ch)
 {
-    microkit_dbg_puts("CLIENT_CORE0|INFO: received a notification on an unexpected channel\n");
+    microkit_dbg_puts("CLIENT_CORE0|INFO: received SGI from core 1\n");
+
+    /* message the server */
+    int runs = 3;
+    while (runs--) {
+        microkit_dbg_puts("client 0 - call server on 0\n");
+        (void) microkit_ppcall(SERVER_CH, microkit_msginfo_new(1, 1));
+    }
 }
--- multikernel-passive-server/client_core1.c
+++ multikernel-passive-server/client_core1.c
@@ -7,20 +7,23 @@
 #include <microkit.h>
 
 #define SERVER_CH 0
+#define REMOTE_CH 1
 
 void init(void)
 {
     microkit_dbg_puts("CLIENT_CORE1|INFO: init function running\n");
-
-    /* message the server */
-    int runs = 40;
-    while (runs--) {
-        microkit_dbg_puts("client 1 - call server on 1\n");
-        (void) microkit_ppcall(SERVER_CH, microkit_msginfo_new(1, 1));
-    }
 }
 
 void notified(microkit_channel ch)
 {
-    microkit_dbg_puts("CLIENT_CORE1|INFO: received a notification on an unexpected channel\n");
+    microkit_dbg_puts("CLIENT_CORE1|INFO: received SGI from core 0\n");
+
+    /* message the server */
+    int runs = 3;
+    while (runs--) {
+        microkit_dbg_puts("client 1 - call server on 1\n");
+        (void) microkit_ppcall(SERVER_CH, microkit_msginfo_new(1, 1));
+    }
+
+    microkit_notify(REMOTE_CH);
 }
