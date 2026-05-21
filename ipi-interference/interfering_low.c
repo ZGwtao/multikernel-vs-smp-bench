05025b86db50c10611df859f7b10c59f8248f1e5
Guangtao Zhu <guangtaozhu@outlook.com>
2026-05-21 12:10:38 +1000

    ipi-interference: add interfering thread on each core
    Signed-off-by: Guangtao Zhu <guangtaozhu@outlook.com>

---
+++ ipi-interference/interfering_low.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2026, UNSW
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#include <stdint.h>
+#include <stddef.h>
+#include <microkit.h>
+#include <assert.h>
+
+#include "benchmark.h"
+#include "config.h"
+
+#define PPC_HI_LO_CHANNEL 2
+
+uintptr_t results;
+
+void init(void)
+{
+    print("interferring client\n");
+    for (;;) {
+        {
+#if 1
+            seL4_BenchmarkNullSyscall();
+#else
+            /* Call high (does not switch threads) */
+            seL4_Call(BASE_ENDPOINT_CAP + PPC_HI_LO_CHANNEL, microkit_msginfo_new(0, 0));
+#endif
+        }
+    }
+}
+
+DECLARE_SUBVERTED_MICROKIT()
